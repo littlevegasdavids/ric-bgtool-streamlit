a6367e6913392c6063f2aed8d859d710587b3875
Reno Davids <reno.davids@sovtech.com>
2025-05-08 09:11:10 +0200

    Changed enviroment names

--- Solver.py
+++ Solver.py
@@ -33,10 +33,10 @@ import time
 #Tag Reno: Enviroment Variables
 load_dotenv()
 db_conn = psycopg2.connect(
-    host=os.getenv('DB_HOST'),
-    database=os.getenv('DB_DATABASE'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD')
+    host=os.getenv('PGHOST'),
+    database=os.getenv('PGDATABASE'),
+    user=os.getenv('PGUSER'),
+    password=os.getenv('PGPASSWORD')
 )
 db_cur = db_conn.cursor()
 
